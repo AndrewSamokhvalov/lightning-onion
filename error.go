3fe194ee3ba7d02994f6c6b5ed9715e994497a8b
Andrey Samokhvalov <andrew.shvv@gmail.com>
2017-06-28 14:12:13 +0300

    sphinx: create disctinct variables for speciication errors
    In order to properly handle errors during decoding and parsing process
    the distinct variables of errors have been created.

--- error.go
+++ error.go
@@ -7,4 +7,17 @@ var (
 	// during processing due to being an attempted replay or probing
 	// attempt.
 	ErrReplayedPacket = fmt.Errorf("sphinx packet replay attempted")
+
+	// ErrInvalidOnionVersion is returned during decoding of the onion packet, when
+	// received packet has unknown version byte.
+	ErrInvalidOnionVersion = fmt.Errorf("invalid onion packet version")
+
+	// ErrInvalidOnionHMAC is returned during onion parsing process, when received
+	// mac do not corresponds to the generated one.
+	ErrInvalidOnionHMAC = fmt.Errorf("invalid mismathed mac")
+
+	// ErrInvalidOnionKey is returned during onion parsing process, when
+	// onion key is invalid - not belongs to the secp256k1 curve.
+	ErrInvalidOnionKey = fmt.Errorf("invalid onion key: pubkey isn't on " +
+		"secp256k1 curve")
 )
